5c564847ffe0a8bc7fc5503a066bc522dd0fdc96
David Kouvchinov <dk@karla.cbmr-ccu.lan.ku.dk>
2024-04-23 21:24:03 +0200

    Initial pytorch refactored model that should function like tf. UNTESTED

---
+++ scripts_2/ML/ModelsPytorch.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class PytorchRefactoredModel(nn.Module):
+    """
+    Model based on my understanding that this model was otherwise hardcoded into the version of Deep Docking I have used previously.
+
+    """
+
+    def __init__(self, input_shape, hyperparameters):
+        super(OriginalModel, self).__init__()
+        self.layers = nn.ModuleList()
+        units = hyperparameters["num_units"]
+        dropout_rate = hyperparameters["dropout_rate"]
+
+        for i, layer_type in enumerate(hyperparameters["bin_array"]):
+            if layer_type == 0:
+                self.layers.append(
+                    nn.Sequential(
+                        nn.Linear(units if i > 0 else input_shape, units),
+                        nn.BatchNorm1d(units),
+                        nn.ReLU(),
+                    )
+                )
+            else:
+                self.layers.append(nn.Dropout(dropout_rate))
+
+        self.output = nn.Linear(units, 1)
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return torch.sigmoid(self.output(x))  # Use sigmoid for binary classification
